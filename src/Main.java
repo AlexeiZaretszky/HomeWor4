d859ece2b2581766e50de98651c06814d713d875
Alexey <ACKOMBAT@yandex.ru>
2022-08-11 19:36:26 +0300

    Выполнил дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        //Задание 1
+        {
+
+
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(i);
+            }
+            System.out.println("\n");
+            int i = 10;
+            while (i > 0) {
+                System.out.println(i);
+                i--;
+            }
+
+
+            //Задание 2
+
+
+            System.out.println("\n");
+            int friday = 5;
+            for (friday = 5; friday < 31; friday = friday + 7) {
+                System.out.println("Сегодня пятница " + friday + " Число " + "Необходимо подготовит отчёт ");
+
+            }
+
+            // Здание 3
+
+            for(var k = 1817; k <=2021;k = k + 79){
+                System.out.println(k+79);
+
+
+
+            }
+}{
+        }
+    } }
